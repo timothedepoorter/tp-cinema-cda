ebbc14bb71d4f40dac5efe2882a45ed9576212c3
Timothé DEPOORTER <depoortertimothe1@gmail.com>
2024-01-31 14:40:41 +0100

    remove useless import

--- src/main/java/fr/timothe/cinemacda4/repository/FilmRepository.java
+++ src/main/java/fr/timothe/cinemacda4/repository/FilmRepository.java
@@ -1,5 +1,6 @@
 package fr.timothe.cinemacda4.repository;
 
+import fr.timothe.cinemacda4.entity.Acteur;
 import fr.timothe.cinemacda4.entity.Film;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
