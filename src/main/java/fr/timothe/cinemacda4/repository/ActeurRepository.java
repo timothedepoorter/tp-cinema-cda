ead38324c446b02625cc7589e041af0bedc858ef
Timothé DEPOORTER <depoortertimothe1@gmail.com>
2024-01-31 14:40:27 +0100

    remove useless import

--- src/main/java/fr/timothe/cinemacda4/repository/ActeurRepository.java
+++ src/main/java/fr/timothe/cinemacda4/repository/ActeurRepository.java
@@ -4,6 +4,8 @@ import fr.timothe.cinemacda4.entity.Acteur;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 public interface ActeurRepository extends JpaRepository<Acteur, Integer> {
 }
