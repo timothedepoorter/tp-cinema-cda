69835d911453ec77ccad090dc9e4e6ea1581f877
Timothé DEPOORTER <depoortertimothe1@gmail.com>
2024-02-01 15:17:07 +0100

    Create SalleRepository

---
+++ src/main/java/fr/timothe/cinemacda4/repository/SalleRepository.java
@@ -0,0 +1,7 @@
+package fr.timothe.cinemacda4.repository;
+
+import fr.timothe.cinemacda4.entity.Salle;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SalleRepository extends JpaRepository<Salle, Integer> {
+}
