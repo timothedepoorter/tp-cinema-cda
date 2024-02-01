dc07472b47d3d454cdd0efa6cd38f7cd90c7adcc
Timothé DEPOORTER <depoortertimothe1@gmail.com>
2024-02-01 15:19:53 +0100

    GET /salles

--- src/main/java/fr/timothe/cinemacda4/controller/SalleController.java
+++ src/main/java/fr/timothe/cinemacda4/controller/SalleController.java
@@ -1,8 +1,13 @@
 package fr.timothe.cinemacda4.controller;
 
+import fr.timothe.cinemacda4.entity.Salle;
 import fr.timothe.cinemacda4.service.SalleService;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/salles")
@@ -11,5 +16,10 @@ public class SalleController {
 
     public SalleController(SalleService salleService) {
         this.salleService = salleService;
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Salle> findAll() {
+        return this.salleService.findAll();
     }
 }
--- src/main/java/fr/timothe/cinemacda4/service/SalleService.java
+++ src/main/java/fr/timothe/cinemacda4/service/SalleService.java
@@ -1,7 +1,10 @@
 package fr.timothe.cinemacda4.service;
 
+import fr.timothe.cinemacda4.entity.Salle;
 import fr.timothe.cinemacda4.repository.SalleRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SalleService {
@@ -9,5 +12,9 @@ public class SalleService {
 
     public SalleService(SalleRepository salleRepository) {
         this.salleRepository = salleRepository;
+    }
+
+    public List<Salle> findAll() {
+        return this.salleRepository.findAll();
     }
 }
