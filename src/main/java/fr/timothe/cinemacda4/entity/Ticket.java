cfc64043ec920f61df6b7012a5a050a5349d88f3
Timothé DEPOORTER <depoortertimothe1@gmail.com>
2024-02-01 16:18:41 +0100

    Create Ticket entity (no relation with Seance)

---
+++ src/main/java/fr/timothe/cinemacda4/entity/Ticket.java
@@ -0,0 +1,23 @@
+package fr.timothe.cinemacda4.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "ticket")
+public class Ticket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String nomClient;
+
+    @Column(nullable = false)
+    private Integer nombrePlaces;
+}
