da21dd3b9a5f3dd6fb6120c35c973be9dbf4cd27
Timothé DEPOORTER <depoortertimothe1@gmail.com>
2024-02-01 16:31:14 +0100

    GET /tickets/{id}

--- src/main/java/fr/timothe/cinemacda4/service/TicketService.java
+++ src/main/java/fr/timothe/cinemacda4/service/TicketService.java
@@ -2,7 +2,9 @@ package fr.timothe.cinemacda4.service;
 
 import fr.timothe.cinemacda4.entity.Ticket;
 import fr.timothe.cinemacda4.repository.TicketRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -16,5 +18,14 @@ public class TicketService {
 
     public List<Ticket> findAll() {
         return this.ticketRepository.findAll();
+    }
+
+    public Ticket findById(Integer id) {
+        return this.ticketRepository.findById(id).orElseThrow(
+                () -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND,
+                        "Aucun ticket pour cet id : " + id
+                )
+        );
     }
 }
