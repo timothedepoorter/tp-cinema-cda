bcc285db257bc436d6ec2615cbb1d14decfd1067
Timothé DEPOORTER <depoortertimothe1@gmail.com>
2024-02-01 16:21:28 +0100

    Create TicketService

---
+++ src/main/java/fr/timothe/cinemacda4/service/TicketService.java
@@ -0,0 +1,13 @@
+package fr.timothe.cinemacda4.service;
+
+import fr.timothe.cinemacda4.repository.TicketRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TicketService {
+    private final TicketRepository ticketRepository;
+
+    public TicketService(TicketRepository ticketRepository) {
+        this.ticketRepository = ticketRepository;
+    }
+}
