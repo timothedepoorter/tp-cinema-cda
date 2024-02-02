b994bfdafab051e74c04caf089805f4ebba651fb
Timothé DEPOORTER <depoortertimothe1@gmail.com>
2024-02-02 16:10:43 +0100

    Add verification to method save() (if film exist)

--- src/main/java/fr/timothe/cinemacda4/service/SeanceService.java
+++ src/main/java/fr/timothe/cinemacda4/service/SeanceService.java
@@ -2,10 +2,8 @@ package fr.timothe.cinemacda4.service;
 
 import fr.timothe.cinemacda4.entity.Salle;
 import fr.timothe.cinemacda4.entity.Seance;
-import fr.timothe.cinemacda4.repository.SalleRepository;
 import fr.timothe.cinemacda4.repository.SeanceRepository;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -14,13 +12,13 @@ import java.util.List;
 @Service
 public class SeanceService {
     private final SeanceRepository seanceRepository;
-    private final SalleRepository salleRepository;
     private final SalleService salleService;
+    private final FilmService filmService;
 
-    public SeanceService(SeanceRepository seanceRepository, SalleRepository salleRepository, SalleService salleService) {
+    public SeanceService(SeanceRepository seanceRepository, SalleService salleService, FilmService filmService) {
         this.seanceRepository = seanceRepository;
-        this.salleRepository = salleRepository;
         this.salleService = salleService;
+        this.filmService = filmService;
     }
 
     public List<Seance> findAll() {
@@ -38,6 +36,7 @@ public class SeanceService {
 
     public Seance save(Seance seance) {
         Salle salle = salleService.findById(seance.getSalle().getId());
+        filmService.findById(seance.getFilm().getId());
         seance.setPlacesDisponibles(salle.getCapacite());
         this.seanceRepository.save(seance);
         return seance;
@@ -45,6 +44,7 @@ public class SeanceService {
 
     public Seance update(Seance seance, Integer id) {
         Salle salle = salleService.findById(seance.getSalle().getId());
+        filmService.findById(seance.getFilm().getId());
         this.findById(id);
         seance.setPlacesDisponibles(salle.getCapacite());
         seance.setId(id);
