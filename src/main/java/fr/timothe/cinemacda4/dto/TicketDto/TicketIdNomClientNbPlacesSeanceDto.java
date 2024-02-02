119ec96c001f47a6469c1ac5fd28a987b231c6f2
Timothé DEPOORTER <depoortertimothe1@gmail.com>
2024-02-02 13:43:39 +0100

    Create DTO for Ticket

---
+++ src/main/java/fr/timothe/cinemacda4/dto/TicketDto/TicketIdNomClientNbPlacesSeanceDto.java
@@ -0,0 +1,11 @@
+package fr.timothe.cinemacda4.dto.TicketDto;
+
+import fr.timothe.cinemacda4.dto.SeanceDto.SeanceIdDatePrixDto;
+import lombok.Data;
+
+@Data
+public class TicketIdNomClientNbPlacesSeanceDto {
+    private Integer id;
+    private String nomClient;
+    private SeanceIdDatePrixDto seance;
+}
