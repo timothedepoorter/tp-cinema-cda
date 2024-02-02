a490928c50df65c47296ef3c3ae853d136c32ff2
Timothé DEPOORTER <depoortertimothe1@gmail.com>
2024-02-02 15:35:43 +0100

    Create DTO for Salle

---
+++ src/main/java/fr/timothe/cinemacda4/dto/SalleDto/SalleIdNumCapaciteDto.java
@@ -0,0 +1,10 @@
+package fr.timothe.cinemacda4.dto.SalleDto;
+
+import lombok.Data;
+
+@Data
+public class SalleIdNumCapaciteDto {
+    private Integer id;
+    private Integer numero;
+    private Integer capacite;
+}
