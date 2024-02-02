baee37210e5eb8ea11ad6e3813f428a46afd8892
Timothé DEPOORTER <depoortertimothe1@gmail.com>
2024-02-02 14:39:19 +0100

    Add verification to save() (if salle exists)

--- src/main/java/fr/timothe/cinemacda4/service/SeanceService.java
+++ src/main/java/fr/timothe/cinemacda4/service/SeanceService.java
@@ -1,6 +1,7 @@
 package fr.timothe.cinemacda4.service;
 
 import fr.timothe.cinemacda4.entity.Seance;
+import fr.timothe.cinemacda4.repository.SalleRepository;
 import fr.timothe.cinemacda4.repository.SeanceRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -12,9 +13,13 @@ import java.util.List;
 @Service
 public class SeanceService {
     private final SeanceRepository seanceRepository;
+    private final SalleRepository salleRepository;
+    private final SalleService salleService;
 
-    public SeanceService(SeanceRepository seanceRepository) {
+    public SeanceService(SeanceRepository seanceRepository, SalleRepository salleRepository, SalleService salleService) {
         this.seanceRepository = seanceRepository;
+        this.salleRepository = salleRepository;
+        this.salleService = salleService;
     }
 
     public List<Seance> findAll() {
@@ -31,11 +36,13 @@ public class SeanceService {
     }
 
     public Seance save(Seance seance) {
+        salleService.findById(seance.getSalle().getId());
         this.seanceRepository.save(seance);
         return seance;
     }
 
     public Seance update(Seance seance, Integer id) {
+        salleService.findById(seance.getSalle().getId());
         this.findById(id);
         seance.setId(id);
         return this.save(seance);
