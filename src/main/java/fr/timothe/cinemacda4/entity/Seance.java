fdcc1999d2812fc57c7c34430fba32a7fc1f08fd
Timothé DEPOORTER <depoortertimothe1@gmail.com>
2024-02-02 09:40:51 +0100

    Create Seance entity (without relations)

---
+++ src/main/java/fr/timothe/cinemacda4/entity/Seance.java
@@ -0,0 +1,25 @@
+package fr.timothe.cinemacda4.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;Create
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "seance")
+public class Seance {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Column(nullable = false)
+    private Integer placesDisponibles;
+}
