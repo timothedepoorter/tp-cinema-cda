0ec6b669c60ac3c80b5749259e83e171c67fdd59
Timothé DEPOORTER <depoortertimothe1@gmail.com>
2024-02-02 13:43:59 +0100

    Return DTO in our controller

--- src/main/java/fr/timothe/cinemacda4/controller/TicketController.java
+++ src/main/java/fr/timothe/cinemacda4/controller/TicketController.java
@@ -1,5 +1,7 @@
 package fr.timothe.cinemacda4.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import fr.timothe.cinemacda4.dto.TicketDto.TicketIdNomClientNbPlacesSeanceDto;
 import fr.timothe.cinemacda4.entity.Ticket;
 import fr.timothe.cinemacda4.service.TicketService;
 import org.springframework.http.HttpStatus;
@@ -12,30 +14,47 @@ import java.util.List;
 @RequestMapping(path = "/tickets")
 public class TicketController {
     private final TicketService ticketService;
+    private final ObjectMapper objectMapper;
 
-    public TicketController(TicketService ticketService) {
+    public TicketController(TicketService ticketService, ObjectMapper objectMapper) {
         this.ticketService = ticketService;
+        this.objectMapper = objectMapper;
     }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Ticket> findAll() {
-        return this.ticketService.findAll();
+    public List<TicketIdNomClientNbPlacesSeanceDto> findAll() {
+        List<Ticket> tickets = this.ticketService.findAll();
+        return tickets.stream().map(
+                ticket -> objectMapper.convertValue(
+                        ticket,
+                        TicketIdNomClientNbPlacesSeanceDto.class
+                )
+        ).toList();
     }
 
     @GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Ticket findById(@PathVariable Integer id) {
-        return this.ticketService.findById(id);
+    public TicketIdNomClientNbPlacesSeanceDto findById(@PathVariable Integer id) {
+        return objectMapper.convertValue(
+                this.ticketService.findById(id),
+                TicketIdNomClientNbPlacesSeanceDto.class
+        );
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
-    public Ticket save(@RequestBody Ticket ticket) {
-        return this.ticketService.save(ticket);
+    public TicketIdNomClientNbPlacesSeanceDto save(@RequestBody Ticket ticket) {
+        return objectMapper.convertValue(
+                this.ticketService.save(ticket),
+                TicketIdNomClientNbPlacesSeanceDto.class
+        );
     }
 
     @PutMapping(path = "/{id}" ,consumes = MediaType.APPLICATION_JSON_VALUE)
-    public Ticket update(@RequestBody Ticket ticket, @PathVariable Integer id) {
-        return this.ticketService.update(ticket, id);
+    public TicketIdNomClientNbPlacesSeanceDto update(@RequestBody Ticket ticket, @PathVariable Integer id) {
+        return objectMapper.convertValue(
+                this.ticketService.update(ticket, id),
+                TicketIdNomClientNbPlacesSeanceDto.class
+        );
     }
 
     @DeleteMapping(path = "/{id}")
